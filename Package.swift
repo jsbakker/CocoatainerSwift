9b99309bbda882c5f18f6052221421d86d30fd67
Jeffrey Bakker <jsbdev@outlook.com>
2026-02-22 13:46:44 -0800

    Add swift package info.

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "CocoatainerSwift",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "CocoatainerSwift",
+            targets: ["CocoatainerSwift"]),
+    ],
+    targets: [
+        .target(
+            name: "CocoatainerSwift",
+            dependencies: [],
+            path: "CocoatainerSwift/CocoatainerSwift"),
+        .testTarget(
+            name: "CocoatainerSwiftTests",
+            dependencies: ["CocoatainerSwift"],
+            path: "CocoatainerSwift/CocoatainerSwiftTests"),
+    ]
+)
